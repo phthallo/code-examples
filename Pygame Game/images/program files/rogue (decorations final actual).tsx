17ad6e7de2ae806140e9af68396001ff70c6adcd
phthallo <84078890+phthallo@users.noreply.github.com>
2022-09-28 12:38:19 +0930

    Add files via upload

---
+++ Pygame Game/images/program files/rogue (decorations final actual).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="rogue decorations final" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="../../../../RF_Catacombs_v1.0/decorative.png" width="768" height="768"/>
+</tileset>
---
+++ Pygame Game/images/program files/statue_actual.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="statue_actual" tilewidth="96" tileheight="120" tilecount="1" columns="1">
+ <image source="../../Pygame Game/images/statue.png" width="96" height="120"/>
+</tileset>
